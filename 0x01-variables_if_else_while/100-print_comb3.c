cda3256d84c5be28f3f3b819a87719c667feb6ab
Jennifer Osato <st.jennyandy@gmail.com>
2022-12-09 19:11:31 -0800

    two digit combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b = 0;
+
+	while (b < 10)
+	{
+		a = 0;
+		while (a < 10)
+		{
+			if (b != a && b < a)
+			{
+				putchar('0' + b);
+				putchar('0' + a);
+				if (a + b != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				};
+			}
+		a++;
+		}
+	b++;
+	}
+}
