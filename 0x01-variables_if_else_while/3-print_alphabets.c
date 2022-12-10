0d73c95bcc87c3ffb352da97b0130a3dde6c7cd0
Jennifer Osato <st.jennyandy@gmail.com>
2022-12-09 17:18:45 -0800

    upper and lower case alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		putchar(az);
+	}
+
+	for (az = 'A'; az <= 'Z'; az++)
+	{
+		putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
