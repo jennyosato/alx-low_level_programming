c98a3d4af048f6fcb6da51279cc5a5cf978c512b
Jennifer Osato <st.jennyandy@gmail.com>
2022-12-13 18:32:38 -0800

    alphabet *10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet ten times
+ * Return: No return
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char j;
+
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(i);
+		i++;
+	};
+}
