1bc5dfc079551df7aece755eefa1f90643929f9c
Jennifer Osato <st.jennyandy@gmail.com>
2022-12-13 19:17:27 -0800

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.c"
+
+/**
+ * print_sign - prints and returns the sign of a number
+ * @n: int
+ * Return: return 1 for positve number, 0 for zero and -1 for negative number
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
