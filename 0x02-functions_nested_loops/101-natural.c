86f47faaf45e80e7af2af47f13e1b13e82c87bf6
Jennifer Osato <st.jennyandy@gmail.com>
2022-12-14 16:45:33 -0800

    multiple of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: int
+ */
+int main(void)
+{
+	int x, y = 0;
+
+	while (x < 1024)
+	{
+		if ((x % 3 == 0) || (x % 5 == 0))
+		{
+			y += x;
+		}
+		x++;
+	}
+	printf("%d\n", y);
+	return (0);
+}
