f482e1ee68c8b8a0b93780a755b43a59f3886b0c
Jennifer Osato <st.jennyandy@gmail.com>
2022-12-19 18:09:32 -0800

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,13 +8,13 @@ void puts_half(char *str)
 {
 	int len = 0;
 
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
 		len++;
 		str++;
 	}
 	str -= (len / 2);
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
 		_putchar(*str);
 		str++;
